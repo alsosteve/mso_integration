76f3741d52641feee7dd2e9be686c4b3d9c590b9
Steve Ngo <stevehngo@hotmail.com>
2026-05-19 18:04:43 -0400

    init mso collection skript

---
+++ scripts/mso_api_collection.py
@@ -0,0 +1,10 @@
+import requests
+from config import API_BASE_URL
+
+url = f"{API_BASE_URL}/search.json?q=propublica"
+
+# API call
+response = requests.get(url)
+
+print(response.status_code)
+print(response.text[:500])
